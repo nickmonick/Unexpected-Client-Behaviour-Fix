51245c48884ca1f621c1e421c42a0869392ac096
skidkidnick <xboxplayzgaming@gmail.com>
2021-12-03 13:05:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import os
+import asyncio
+print("Tool made by ! Grim#1811")
 username = os.environ['USERPROFILE']
 local = "\AppData\Local\Roblox\\"
 location = username + local
 path = os.path.join(location, "GlobalBasicSettings_13.xml")
-print (path)
-os.remove(path)
+if os.path.exists(path):
+    os.remove(path)
+    print ("Success Fully Removed {path}")
+else:
+    print("File does not exist or has already been deleted")
+asyncio.sleep(4)
+
